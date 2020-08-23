29b06adb030d97c1fc31a9494a4d3dcd3ca072c3
Akshay Vadher <ce.akshay@gmail.com>
2020-08-23 21:24:41 +0530

    Auto deployment added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  providers: [{
+    provide: APP_BASE_HREF, useValue: '/crypto/'
+  }]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
